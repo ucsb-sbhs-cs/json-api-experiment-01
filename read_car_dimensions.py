fb0cce79b5b9d75a04f2856a15a8e5b5c884f5c3
Phill Conrad <pconrad@cs.ucsb.edu>
2017-10-03 16:05:43 -0700

    Work In Progress PC/YM try this api

--- read_car_dimensions.py
+++ read_car_dimensions.py
@@ -1,7 +1,10 @@
 import json
-from pprint import pprint
+import json
 
 with open('car_dimensions.json') as data_file:    
     data = json.load(data_file)
 
-pprint(data)
+print("The width of the car is: ",data["Width"]," cm")
+print("The height of the car is: ",data["Height"]," cm")
+print("The length of the car is: ",data["Length"]," cm")
+
